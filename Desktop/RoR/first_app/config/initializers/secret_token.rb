3f057460f6f5c20db6f14d048d57a7a37511097e
Ming Fang <Ming.Fang@eicsson.com>
2013-04-30 17:12:00 -0400

    first commit

---
+++ Desktop/RoR/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '96ced8a46783cbe09cfa430473086d8b508cec50eab1cfd6eb1cdab5a8b619d9bcf92f4e3d57139ff8d46f68c1fefeb39b7439a44272a1abfacc68a527a4e96f'
